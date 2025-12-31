42c40a584dfa574f1a37fbc401272b5afdc0a768
igorrm23 <igormachadofire14@gmail.com>
2025-12-31 18:14:34 -0300

    rajustando layout

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const span1 = document.querySelector('#span1');
 const span2 = document.querySelector('#span2');
 const span3 = document.querySelector('#span3');
 const button = document.getElementById('button');
+const insight = document.getElementById('insight');
+const stat = document.getElementById('#status');
 
 range1.addEventListener('input', function () {
     span1.textContent = this.value;
@@ -13,6 +15,16 @@ range2.addEventListener('input', function () {
     span2.textContent = this.value;
 });
 
+
 button.addEventListener('click', function () {
-    span3.textContent = range1.value * range2.value / 100;
+    span3.textContent = ((range2.value / range1.value) * 100).toFixed(3);
+    const porcentagem = span3.textContent;
+
+    if (porcentagem < 2.999) {
+        insight.style.backgroundColor = 'red';
+        stat.innerText = 'Baixo';
+    } else if (porcentagem > 10.999) {
+        insight.style.backgroundColor = 'green';
+        stat.innerText = 'Alto';
+    }
 });
